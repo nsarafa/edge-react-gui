fd8b44a499a652fc40ad88eed4e458073158e811
Art <artmanalili@gmail.com>
2017-07-15 14:59:46 +0800

    Commited some experimentation, a bug is making the app so slow

--- src/modules/UI/components/SendConfirmationOptions/ChangeCurrency.js
+++ src/modules/UI/components/SendConfirmationOptions/ChangeCurrency.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Modal, TouchableHighlight } from 'react-native'
 import { Content, Form, Picker, Item } from 'native-base'
 import { connect } from 'react-redux'
 
@@ -8,23 +8,57 @@ class ChangeCurrency extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "chilly"
+      text: "chilly",
+      modalVisible: false,
+      measurement: {}
     };
   }
+
+  componentDidMount () {
+    this.textInput.measure((ox, oy, width, height, px, py) => {
+      this.state.measurement = { ox, oy, width, height, px, py }
+    })
+  }
+
   onValueChange(value: string) {
     this.setState({
       selected1: value
     });
   }
 
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+
   render () {
+
     return (
       <Content>
+
+        <Modal
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+        >
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+              <TouchableHighlight onPress={() => {this.setModalVisible(!this.state.modalVisible)}}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+
+          </View>
+        </View>
+        </Modal>
+
         <View style={{ flex: 1, padding: 50 }}>
           <TextInput
             style={{height: 40, borderColor: 'gray', borderWidth: 1}}
             onChangeText={(text) => this.setState({text})}
+            onFocus={() => {this.setModalVisible(true)}}
             value={this.state.text}
+            ref={(input) => { this.textInput = input; }}
           />
         </View>
       </Content>
